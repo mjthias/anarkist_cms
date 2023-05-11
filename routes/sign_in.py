aa07ae3bce6232e30c1213374b8c4c963bcb17e1
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2023-05-11 14:59:34 +0200

    Created simple login feature

--- routes/sign_in.py
+++ routes/sign_in.py
@@ -1,7 +1,16 @@
-from bottle import get, view
+from bottle import get, view, redirect, request
+from utils.g import _RESPOND
+from utils.vars import JWT_SECRET
+import jwt
 
 ##############################
 @get("/sign-in")
 @view("sign_in")
 def _():
+    if request.get_cookie("anarkist"):
+        cookie = request.get_cookie("anarkist")
+        decoded_jwt = jwt.decode(cookie, JWT_SECRET, algorithms=["HS256"])
+
+        return redirect("/")
+
     return 
---
+++ routes/sign_in_post.py
@@ -0,0 +1,49 @@
+from bottle import post, redirect, request, response
+from utils.g import _RESPOND
+from utils.vars import DB_CONFIG, JWT_SECRET
+import pymysql
+import time
+import jwt
+
+##############################
+@post("/sign-in")
+def _():
+    try:
+        user_email = request.forms.get("user_email")
+        user_password = request.forms.get("user_password")
+
+        db_connect = pymysql.connect(**DB_CONFIG)
+        db_connect.begin()
+        cursor = db_connect.cursor()
+
+        cursor.execute("SELECT * FROM users WHERE user_email = %s LIMIT 1", (user_email))
+        user = cursor.fetchone()
+
+        if not user: return _RESPOND(400, "User does not exist.")
+        if not user_password == user["user_password"]: return _RESPOND(400, "Password does not match.")
+
+        session = {
+            "fk_user_id": user["user_id"],
+            "session_iat": int(time.time()),
+        }
+
+        query = """
+            INSERT INTO sessions (fk_user_id, session_iat)
+            VALUES(%s, %s)
+        """
+
+        cursor.execute(query, (session["fk_user_id"], session["session_iat"]))
+        session["session_id"] = cursor.lastrowid
+
+        encoded_jwt = jwt.encode(session, JWT_SECRET, algorithm="HS256")
+        response.set_cookie("anarkist", encoded_jwt, path="/")
+
+        db_connect.commit()
+        return _RESPOND(200, "Session successfully created.")
+    except Exception as ex:
+        print(str(ex))
+        db_connect.rollback()
+        return _RESPOND(500, "Server error")
+    finally:
+        cursor.close()
+        db_connect.close()
