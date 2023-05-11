9fc41853dd054e456d86a432f1cd41cbe28de733
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2023-05-11 14:58:23 +0200

    moved fetching of beers to beers.py

--- routes/home.py
+++ routes/home.py
@@ -1,27 +1,6 @@
-from bottle import get, response
-from utils.vars import DB_CONFIG
-import json
-import pymysql
+from bottle import get
 
 ##############################
-
 @get("/")
 def _():
-    try:
-        db = pymysql.connect(**DB_CONFIG)
-        cursor = db.cursor()
-        # cursor.execute("SELECT * FROM beers")
-        # cursor.execute("SELECT * FROM beer_list")
-        # cursor.execute("CALL get_beer_by_name (%s)", ("american haze",))
-        cursor.execute("CALL get_beer_by_fuzzy_name (%s)", ("amricn hase",))
-        beers = cursor.fetchall()
-        return json.dumps(beers)
-    
-    except Exception as ex:
-        print(str(ex))
-        response.status = 500
-        return "Server error"
-
-    finally:
-        cursor.close()
-        db.close()
+    return "Welcome!"
