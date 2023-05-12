a5d1631c4cf0988e7ad4b8cbec64a659cf10ab8f
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2023-05-12 12:31:36 +0200

    AB-60 Now a GET method, and redirects to 'sign-in'

--- routes/sign_out.py
+++ routes/sign_out.py
@@ -1,17 +1,19 @@
-from bottle import post, redirect, request, response
+from bottle import get, redirect, request, response
 from utils.g import _DELETE_SESSION, _RESPOND
 from utils.vars import _JWT_SECRET
 import jwt
 
 ##############################
-@post("/sign-out")
+@get("/sign-out")
 def _():
     try:
         cookie = request.get_cookie("anarkist")
         decoded_jwt = jwt.decode(cookie, _JWT_SECRET, algorithms=["HS256"])
         _DELETE_SESSION(decoded_jwt)
         response.set_cookie("anarkist", cookie, path="/", expires=0)
-        return redirect("/sign-in")
+        return _RESPOND(200, "Successfully signed out.")
     except Exception as ex:
         print(str(ex))
         return _RESPOND(500, "Server error.")
+    finally:
+        redirect("/sign-in")
