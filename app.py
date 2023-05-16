b8ff73b19490391a593aca5b323920588c39ffc0
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2023-05-16 12:11:56 +0200

    AB-68 import api users get

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ from bottle import run, default_app
 
 ##############################
 
+# API
+import api.users_get
+
+# CMS
 import routes.sign_in
 import routes.sign_in_post
+import routes.select_location
+import routes.select_location_post
 import routes.sign_out
 import routes.home
 import routes.beers
