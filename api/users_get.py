099c85a94ae29618f3079208af55f4327161d771
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2023-05-16 13:54:53 +0200

    AB-68 Validates input data and checks whether user is super user or not

--- api/users_get.py
+++ api/users_get.py
@@ -1,6 +1,7 @@
 from bottle import get, response, request
 from utils.vars import _DB_CONFIG, _API_PATH, _JWT_SECRET
 from utils.g import _RESPOND
+import utils.validation as validate
 import pymysql
 import json
 import jwt
@@ -15,18 +16,24 @@ def _():
     decoded_jwt = jwt.decode(cookie, _JWT_SECRET, algorithms=["HS256"])
     if not int(decoded_jwt["user_role"]) in authorized_user_roles: return _RESPOND(403, "Unauthorized attempt.")
     
+    bar_id, error = validate._ID(str(decoded_jwt["bar_id"]))
+    if error: return _RESPOND(400, error)
+    offset = request.query.get("offset") if request.query.get("offset") else "0"
+    offset, error = validate._OFFSET(offset)
+    if error: return _RESPOND(400, error)
+    limit = request.query.get("limit") if request.query.get("limit") else "100"
+    limit, error = validate._LIMIT(limit)
+    if error: return _RESPOND(400, error)
+
+    if decoded_jwt["user_role"] == 1:
+        where_clause = "WHERE bar_id = %s OR bar_id IS NULL"
+    else:
+        where_clause = "WHERE bar_id = %s"
+
     try:
         db_connect = pymysql.connect(**_DB_CONFIG)
         cursor = db_connect.cursor()
 
-        limit = int(request.query.get("limit")) if request.query.get("limit") else 10
-        offset = int(request.query.get("offset")) if request.query.get("offset") else 0
-        if int(decoded_jwt["user_role"]) == 1:
-            where_clause = "WHERE bar_id = %s OR bar_id IS NULL"
-        else:
-            where_clause = "WHERE bar_id = %s"
-
-        bar_id = int(decoded_jwt["bar_id"])
         cursor.execute(f"SELECT * FROM users_list {where_clause} LIMIT %s,%s", (bar_id, offset, limit))
         users = cursor.fetchall()
 
