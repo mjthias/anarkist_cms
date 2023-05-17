c86b0fc570b7fbb27a31de4be97ab923f09077b4
Mathias Jensen <mathias.j@forteadvice.com>
2023-05-17 11:50:07 +0200

    rm 'g.' from g... + respond takes lists

--- utils/g.py
+++ utils/g.py
@@ -1,13 +1,15 @@
 from bottle import response
 import pymysql
 import utils.vars as var
+import json
 
 ##############################
-def respond(status = 400, error_message = "Unknown error"):
+def respond(status = 400, data = "Unknown error"):
+    response.content_type = 'application/json'
     response.status = status
-    return {
-        "info": error_message
-    }
+    if type(data) is str:
+        return json.dumps({ "info": data })
+    return json.dumps(data)
 
 ##############################
 def delete_session(session=None):
@@ -26,7 +28,7 @@ def delete_session(session=None):
         print(f"Rows deleted: {counter}")
     except Exception as ex:
         print(str(ex))
-        return g.respond(500, "Server error.")
+        return respond(500, "Server error.")
     finally:
         cursor.close()
         db_connect.close()
@@ -51,7 +53,7 @@ def update_session(now=0, session=None):
         print(f"Rows updated: {counter}")
     except Exception as ex:
         print(str(ex))
-        g.respond(500, "Server error.")
+        respond(500, "Server error.")
     finally:
         cursor.close()
         db_connect.close()
