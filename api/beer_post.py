88b3d986ea637a9765d838775e79989754468c3e
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2023-05-20 23:47:29 +0200

    AB-98 implemented validation of inputs

--- api/beer_post.py
+++ api/beer_post.py
@@ -11,8 +11,50 @@ def _():
     session = validate.session()
     if not session: return g.respond(401, "Unauthorized attempt.")
 
+    # VALIDATE INPUT VALUES
     try:
-        return g.respond(201, "Beer created")
+        allowed_keys = ["beer_name", "brewery_name", "brewery_id", "beer_style_name", "beer_style_id", "beer_alc", "beer_price", "beer_ibu", "beer_ebc", "beer_description_en", "beer_description_dk", "beer_image", "beer_image_name"]
+        for key in request.forms.keys():
+            if not key in allowed_keys: return g.respond(403, f"Forbidden key: {key}")
+        
+        beer_name, error = validate.name(request.forms.get("beer_name"))
+        if error: return g.respond(400, error)
+        brewery_id, error = validate.id(request.forms.get("brewery_id"))
+        if error: return g.respond(400, f"Brewery {error}")
+        beer_style_id, error = validate.id(request.forms.get("beer_style_id"))
+        if error: return g.respond(400, f"Beer style {error}")
+        beer_alc, error = validate.alc(request.forms.get("beer_alc"))
+        if error: return g.respond(400, error)
+        beer_price, error = validate.price(request.forms.get("beer_price"))
+        if error: return g.respond(400, error)
+        beer_ibu, error = validate.ibu(request.forms.get("beer_ibu"))
+        if error: return g.respond(400, error)
+        beer_ebc, error = validate.ebc(request.forms.get("beer_ebc"))
+        if error: return g.respond(400, error)
+        beer_description_en, error = validate.description(request.forms.get("beer_description_en"))
+        if error: return g.respond(400, error)
+        beer_description_dk, error = validate.description(request.forms.get("beer_description_dk"))
+        if error: return g.respond(400, error)
+        if request.files.get("beer_image"):
+            beer_image, error = validate.image(request.files.get("beer_image"))
+            if error: return g.respond(400, error)
+        else:
+            beer_image = ""
+
+        beer = {
+            "beer_name": beer_name,
+            "brewery_id": brewery_id,
+            "beer_style_id": beer_style_id,
+            "beer_alc": beer_alc,
+            "beer_price": beer_price,
+            "beer_ibu": beer_ibu,
+            "beer_ebc": beer_ebc,
+            "beer_description_en": beer_description_en,
+            "beer_description_dk": beer_description_dk,
+            "beer_image": beer_image
+        }
+
+        return g.respond(201, beer)
     except Exception as ex:
         print(str(ex))
         return g.respond(500, "Server error.")
