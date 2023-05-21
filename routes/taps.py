eb959bb0227fc159c384cdeb2839abece1eccb70
Mathias Jensen <mathias.j@forteadvice.com>
2023-05-21 16:36:46 +0200

    AB-107 get tabs from db, return in dict

--- routes/taps.py
+++ routes/taps.py
@@ -1,12 +1,37 @@
 from bottle import get, view, redirect
 import utils.validation as validate
+import utils.vars as var
+import utils.g as g
+import pymysql
 
 ##############################
 
 @get("/taps")
 @view("taps")
 def _():
+    # VALIDATE SESSION
     session = validate.session()
     if not session: return redirect("/sign-in")
+
+    # GET TAPS FROM DB
+    try:
+        db = pymysql.connect(**var.DB_CONFIG)
+        cursor = db.cursor()
+        cursor.execute("""
+            SELECT * FROM taps_list
+            WHERE fk_bar_id = %s
+            """, (session["bar_id"]))
+        taps = cursor.fetchall()
+        
+        return dict(
+            session = session,
+            taps = taps,
+            )
+
+    except Exception as ex:
+        print(ex)
+        return g.respond(500, "Server error")
     
-    return dict(session = session)
+    finally:
+        cursor.close()
+        db.close()
