f56b83019d5965204d66e281ca0f1152c78a514b
Mathias Jensen <mathias.j@forteadvice.com>
2023-05-22 17:04:53 +0200

    AB-107 init route for creating taps

---
+++ routes/taps_create.py
@@ -0,0 +1,20 @@
+from bottle import get, view, redirect
+import utils.validation as validate
+import utils.g as g
+
+
+##############################
+
+@get("/taps/create")
+@view("taps_create")
+def _():
+    # VALIATE SESSION
+    session = validate.session()
+    if not session: return redirect("/sign-in")
+
+    # VALIDATE ROLE
+    if session["role_id"] == 3:
+        return g.respond(401, "Unauthorized attempt")
+    
+    return dict(session=session)
+
