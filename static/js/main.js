1e6d89299b620c7a7ebd1795dd43327862097f17
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2023-05-22 09:36:24 +0200

    AB-105 Initial posting of beer styles implemented

--- static/js/main.js
+++ static/js/main.js
@@ -268,6 +268,23 @@ async function searchBrewery() {
   }
 }
 
+async function postBeerStyle(form) {
+  const conn = await fetch('/api/v1/beer-styles', {
+    method: "POST",
+    body: new FormData(form)
+  });
+
+  if (!conn.ok) {
+    const error = await conn.json();
+    console.log(error);
+    return;
+  }
+
+  const beerStyleId = await conn.json();
+  spa(`/beer-styles/${beerStyleId}`);
+
+}
+
 async function updateBeerStyle(form) {
   const beerStyleId = form.beer_style_id.value;
   const conn = await fetch(`/api/v1/beer-styles/${beerStyleId}`, {
