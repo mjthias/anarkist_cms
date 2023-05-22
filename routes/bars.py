8d46901b492aa6db0b526cdd6f25b17d90620982
Mathias Jensen <mathias.j@forteadvice.com>
2023-05-22 20:20:17 +0200

    AB-109 bars init

---
+++ routes/bars.py
@@ -0,0 +1,40 @@
+from bottle import get, view, redirect
+import utils.validation as validate
+import utils.vars as var
+import utils.g as g
+import pymysql
+
+##############################
+
+@get("/bars")
+@view("bars")
+def _():
+    # VALIDATE SESSION
+    session = validate.session()
+    if not session: return redirect("/sign-in")
+
+    # VALIDATE ROLE
+    if not session["role_id"] == 1:
+        return redirect("/")
+    
+    try:
+        db = pymysql.connect(**var.DB_CONFIG)
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM bars")
+        bars = cursor.fetchall()
+        return dict(
+            session = session,
+            bars = bars
+            )
+
+
+    except Exception as ex:
+        print(ex)
+        return g.respond(500, "Server error")
+
+    finally:
+        cursor.close()
+        db.close()
+
+    
+
