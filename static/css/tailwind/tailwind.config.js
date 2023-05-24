69fb7d6ff32cf45ba9bb149a42dfd56385fd6c46
Mathias Jensen <mathias.j@forteadvice.com>
2023-05-24 17:28:14 +0200

    AB-50 highlight vs hover color

--- static/css/tailwind/tailwind.config.js
+++ static/css/tailwind/tailwind.config.js
@@ -4,19 +4,20 @@ module.exports = {
   content: ["../../../views/*.html", "../../../views/*/*.html"],
   theme: {
     colors: {
-      'danger': "rgba(220,38,37,100)",
-      'success': 'rgba(23,163,74,100)',
-      'background': 'rgba(14, 14,14,100)',
-      'outline': 'rgba(82,82,82,100)',
-      'outline-focus': 'rgba(138,138,138,100)', 
+      'danger': "rgba(220,38,37,1)",
+      'success': 'rgba(23,163,74,1)',
+      'background': 'rgba(14, 14,14,1)',
+      'outline': 'rgba(82,82,82,1)',
+      'outline-focus': 'rgba(138,138,138,1)', 
       'line': 'rgba(71,71,71,100)',
-      'input-bg': 'rgba(45,45,45,100)',
-      'highlight-bg': 'rgba(45,45,45,100)',
-      'primary': 'rgba(255,255,255,100)',
-      'secondary': 'rgba(156,156,156,100)',
+      'input-bg': 'rgba(45,45,45,1)',
+      'highlight-bg': 'rgba(45,45,45,1)',
+      'highlight-bg-hover': 'rgba(45,45,45,0.5)',
+      'primary': 'rgba(255,255,255,1)',
+      'secondary': 'rgba(156,156,156,1)',
       'btn': {
-        'light': 'rgba(80,170,100,100)',
-        'dark': 'rgba(67,146,85,100)',
+        'light': 'rgba(80,170,100,1)',
+        'dark': 'rgba(67,146,85,1)',
         'invalid': 'rgba()'
       }
     },
