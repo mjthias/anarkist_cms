9d39f4c0ca7cfe9d9a90cadc20a04c1e2658b822
Mathias Jensen <mathias.j@forteadvice.com>
2023-05-29 17:17:21 +0200

    AB-140 beer styles search on /beer-styles

--- routes/beer_styles/beer_styles.py
+++ routes/beer_styles/beer_styles.py
@@ -8,6 +8,7 @@ import pymysql
 @get("/beer-styles")
 @view("beer_styles/index")
 def _():
+    # VALIDATE
     session = validate.session()
     if not session: return redirect("/sign-in")
 
@@ -17,14 +18,26 @@ def _():
     offset, error = validate.offset(request.params.get("offset"))
     if error: return g.error_view(404)
 
+    if request.params.get("name"):
+        beer_style_name, error = validate.name(request.params.get("name"))
+        if error: return g.error_view(204)
+    else: beer_style_name = None
+
     try:
         db_connect = pymysql.connect(**var.DB_CONFIG)
         cursor = db_connect.cursor()
-        
-        cursor.execute("""
-        SELECT * FROM beer_styles
-        LIMIT %s, %s
-        """, (offset, limit))
+        # No name search param
+        if not beer_style_name:
+            cursor.execute("""
+            SELECT * FROM beer_styles
+            LIMIT %s, %s
+            """, (offset, limit))
+        # Name search param
+        else:
+            cursor.execute("""
+            CALL get_beer_style_by_fuzzy_name(%s,%s,%s)
+            """, (beer_style_name, offset, limit))
+
         beer_styles = cursor.fetchall()
 
         # Render beer_styles_list.html only?
