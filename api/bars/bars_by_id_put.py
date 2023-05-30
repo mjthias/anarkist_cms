bf208843bf02bed0a1f71b9dddb3c242cc731cc8
Mathias Jensen <mathias.j@forteadvice.com>
2023-05-30 10:59:23 +0200

    AB-151 try catch added

--- api/bars/bars_by_id_put.py
+++ api/bars/bars_by_id_put.py
@@ -6,36 +6,41 @@ from utils import g, validation as validate, vars as var
 
 @put(f"{var.API_PATH}/bars/<bar_id>")
 def _(bar_id):
-    # VALIDATE
-    session = validate.session()
-    if not session:
-        return g.respond(401)
+    try:
+        # VALIDATE
+        session = validate.session()
+        if not session:
+            return g.respond(401)
 
-    if not session["role_id"] == 1:
-        return g.respond(401)
+        if not session["role_id"] == 1:
+            return g.respond(401)
 
-    # bar id param
-    bar_id, error = validate.id(bar_id)
-    if error:
-        return g.respond(400, error)
+        # bar id param
+        bar_id, error = validate.id(bar_id)
+        if error:
+            return g.respond(400, error)
 
-    # Input values
-    bar_name, error = validate.name(request.forms.get("bar_name"))
-    if error:
-        return g.respond(400, error)
+        # Input values
+        bar_name, error = validate.name(request.forms.get("bar_name"))
+        if error:
+            return g.respond(400, error)
 
-    bar_street, error = validate.name(request.forms.get("bar_street"))
-    if error:
-        return g.respond(400, error)
+        bar_street, error = validate.name(request.forms.get("bar_street"))
+        if error:
+            return g.respond(400, error)
 
-    bar_city, error = validate.name(request.forms.get("bar_city"))
-    if error:
-        return g.respond(400, error)
+        bar_city, error = validate.name(request.forms.get("bar_city"))
+        if error:
+            return g.respond(400, error)
 
-    bar_zip_code, error = validate.zip_code(request.forms.get("bar_zip_code"))
-    if error:
-        return g.respond(400, error)
-
+        bar_zip_code, error = validate.zip_code(request.forms.get("bar_zip_code"))
+        if error:
+            return g.respond(400, error)
+        
+    except Exception as ex:
+        print(str(ex))
+        return g.respond(500)
+    
     # UPDATE IN DB
     try:
         db = pymysql.connect(**var.DB_CONFIG)
