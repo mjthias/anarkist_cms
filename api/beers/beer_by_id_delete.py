6d762990485873a3b441e1055d96d101c2b6da06
Mathias Jensen <mathias.j@forteadvice.com>
2023-05-30 11:34:02 +0200

    AB-151 pylint

--- api/beers/beer_by_id_delete.py
+++ api/beers/beer_by_id_delete.py
@@ -1,49 +1,50 @@
+# pylint: disable=W0612
 from bottle import delete, request
-import utils.g as g
-import utils.vars as var
-import utils.validation as validate
 import pymysql
+from utils import g, vars as var, validation as validate
 
 ##############################
 @delete(f"{var.API_PATH}/beers/<beer_id>")
 def _(beer_id=""):
     try:
-        # VALIDATE SESSION
+        # VALIDATE
         session = validate.session()
-        if not session: return g.respond(401)
+        if not session:
+            return g.respond(401)
 
-        # VALIDATE ALLOWED KEYS
-        allowed_keys = ["id", "confirm_deletion"]
-        for key in request.forms.keys():
-            if not key in allowed_keys: return g.respond(403, f"Forbidden key: {key}")
-        
-        # VALIDATE INPUT
+        # Input values
         beer_id, error = validate.id(beer_id)
-        if error: return g.respond(400, f"Beer {error}")
+        if error:
+            return g.respond(400, f"Beer {error}")
+
         confirm_delete, error = validate.confirm_deletion(request.forms.get("confirm_deletion"))
-        if error: return g.respond(400, error)
+        if error:
+            return g.respond(400, error)
 
     except Exception as ex:
         print(str(ex))
-        return g.respond(500, "Server error")
-    
+        return g.respond(500)
+
     # CONNECT TO DB
     try:
         db_connect = pymysql.connect(**var.DB_CONFIG)
         cursor = db_connect.cursor()
 
         cursor.execute("""
-            DELETE FROM beers
-            WHERE beer_id = %s
+        DELETE FROM beers
+        WHERE beer_id = %s
         """, (beer_id,))
         counter = cursor.rowcount
-        if not counter: return g.respond(204, "")
+        if not counter:
+            return g.respond(204, "")
         db_connect.commit()
 
         return g.respond(200, f"Successfully deleted beer with ID: {beer_id}")
+
     except Exception as ex:
         print(str(ex))
-        return g.respond(500, "Server error.")
+        return g.respond(500)
+
     finally:
         cursor.close()
         db_connect.close()
--- api/beers/beer_post.py
+++ api/beers/beer_post.py
@@ -1,47 +1,63 @@
-from  bottle import post, request
-import utils.vars as var
-import utils.g as g
-import utils.validation as validate
-import pymysql
 import time
+from  bottle import post, request
+import pymysql
+from utils import g, vars as var, validation as validate
 
 ##############################
+
 @post(f"{var.API_PATH}/beers")
 def _():
-    # VALIDATE SESSION
-    session = validate.session()
-    if not session: return g.respond(401)
-
-    # VALIDATE INPUT VALUES
     try:
-        allowed_keys = ["beer_name", "brewery_name", "brewery_id", "beer_style_name", "beer_style_id", "beer_alc", "beer_price", "beer_ibu", "beer_ebc", "beer_description_en", "beer_description_dk", "beer_image", "beer_image_name"]
-        for key in request.forms.keys():
-            if not key in allowed_keys: return g.respond(403, f"Forbidden key: {key}")
-        
+        # VALIDATE
+        session = validate.session()
+        if not session:
+            return g.respond(401)
+
+        # Input values
         beer_name, error = validate.name(request.forms.get("beer_name"))
-        if error: return g.respond(400, error)
+        if error:
+            return g.respond(400, error)
+
         brewery_id, error = validate.id(request.forms.get("brewery_id"))
-        if error: return g.respond(400, f"Brewery {error}")
+        if error:
+            return g.respond(400, f"Brewery {error}")
+
         beer_style_id, error = validate.id(request.forms.get("beer_style_id"))
-        if error: return g.respond(400, f"Beer style {error}")
+        if error:
+            return g.respond(400, f"Beer style {error}")
+
         beer_alc, error = validate.alc(request.forms.get("beer_alc"))
-        if error: return g.respond(400, error)
+        if error:
+            return g.respond(400, error)
+
         beer_price, error = validate.price(request.forms.get("beer_price"))
-        if error: return g.respond(400, error)
+        if error:
+            return g.respond(400, error)
+
         beer_ibu, error = validate.ibu(request.forms.get("beer_ibu"))
-        if error: return g.respond(400, error)
+        if error:
+            return g.respond(400, error)
+
         beer_ebc, error = validate.ebc(request.forms.get("beer_ebc"))
-        if error: return g.respond(400, error)
+        if error:
+            return g.respond(400, error)
+
         beer_description_en, error = validate.description(request.forms.get("beer_description_en"))
-        if error: return g.respond(400, error)
+        if error:
+            return g.respond(400, error)
+
         beer_description_dk, error = validate.description(request.forms.get("beer_description_dk"))
-        if error: return g.respond(400, error)
+        if error:
+            return g.respond(400, error)
+
         if request.files.get("beer_image") and not request.files.get("beer_image").filename == "empty":
             beer_image, error = validate.image(request.files.get("beer_image"))
-            if error: return g.respond(400, error)
+            if error:
+                return g.respond(400, error)
         else:
             beer_image = ""
 
+        # Create beer tuple
         beer = (
             beer_name,
             brewery_id,
@@ -62,39 +78,39 @@ def _():
     except Exception as ex:
         print(str(ex))
         return g.respond(500)
-    
-    # CONNECT TO DB
-    try: 
+
+    # POST TO DB
+    try:
         db_connect = pymysql.connect(**var.DB_CONFIG)
         cursor = db_connect.cursor()
-
         query = """
-            INSERT INTO beers
-            (beer_name,
-            fk_brewery_id,
-            beer_ebc,
-            beer_ibu,
-            beer_alc,
-            fk_beer_style_id,
-            beer_price,
-            beer_image,
-            beer_description_en,
-            beer_description_dk,
-            beer_created_at,
-            fk_beer_created_by,
-            beer_updated_at,
-            fk_beer_updated_by)
-            VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        INSERT INTO beers
+        (beer_name,
+        fk_brewery_id,
+        beer_ebc,
+        beer_ibu,
+        beer_alc,
+        fk_beer_style_id,
+        beer_price,
+        beer_image,
+        beer_description_en,
+        beer_description_dk,
+        beer_created_at,
+        fk_beer_created_by,
+        beer_updated_at,
+        fk_beer_updated_by)
+        VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """
-
         cursor.execute(query, beer)
         beer_id = cursor.lastrowid
         db_connect.commit()
 
         return g.respond(201, beer_id)
+
     except Exception as ex:
         print(str(ex))
         return g.respond(500)
+
     finally:
         cursor.close()
         db_connect.close()
